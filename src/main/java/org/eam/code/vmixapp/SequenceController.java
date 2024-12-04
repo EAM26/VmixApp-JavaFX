daea93beceead6e9495f11038e8902615e34d1d2
EAM26 <emuller2607@gmail.com>
2024-12-04 15:29:25 +0100

    Add styling and fx id's to elements and copy to SequenceController

--- src/main/java/org/eam/code/vmixapp/SequenceController.java
+++ src/main/java/org/eam/code/vmixapp/SequenceController.java
@@ -1,4 +1,60 @@
 package org.eam.code.vmixapp;
 
-public class SequenceController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SequenceController implements Initializable {
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private TableView<Sequence> table;
+
+    @FXML
+    private TextField tfDescription;
+
+    @FXML
+    private TextField tfName;
+
+    @FXML
+    void clearField(ActionEvent event) {
+
+    }
+
+    @FXML
+    void createSequence(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteSequence(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateSequence(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
