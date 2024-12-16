ae76039c59b097daebe04d1c60e503ee9d384b87
EAM26 <emuller2607@gmail.com>
2024-12-16 16:46:47 +0100

    Add Recorder class

---
+++ src/main/java/org/eam/code/vmixapp/model/Recorder.java
@@ -0,0 +1,24 @@
+package org.eam.code.vmixapp.model;
+
+public class Recorder {
+
+    private Scene preview;
+
+    private Scene actual;
+
+    public Scene getPreview() {
+        return preview;
+    }
+
+    public void setPreview(Scene preview) {
+        this.preview = preview;
+    }
+
+    public Scene getActual() {
+        return actual;
+    }
+
+    public void setActual(Scene actual) {
+        this.actual = actual;
+    }
+}
